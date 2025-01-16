58363bafe0c6db2ef1d40baf3c27de176efddcd6
Peter Roddy <Peter.RODDY@EDUCATION.GOV.UK>
2025-01-16 14:40:14 +0000

    csv service abstracted from function - get csv from url added

---
+++ src/SFA.DAS.AODP.Jobs/Interfaces/ICsvReaderService.cs
@@ -0,0 +1,11 @@
+﻿using CsvHelper.Configuration;
+
+namespace SFA.DAS.AODP.Jobs.Interfaces
+{
+    public interface ICsvReaderService
+    {
+        List<T> ReadCSVFromFilePath<T, TMap>(string filePath) where TMap : ClassMap<T>;
+        Task<List<T>> ReadCsvFileFromUrlAsync<T, TMap>(string urlFilePath) where TMap : ClassMap<T>;
+
+    }
+}
---
+++ src/SFA.DAS.AODP.Jobs/Services/CSV/CSVReaderService.cs
@@ -0,0 +1,75 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AODP.Jobs.Interfaces;
+
+namespace SFA.DAS.AODP.Jobs.Services.CSV
+{
+    public class CsvReaderService : ICsvReaderService
+    {
+        private readonly ILogger<CsvReaderService> _logger;
+        private readonly IHttpClientFactory _httpClient;
+
+        public CsvReaderService(ILogger<CsvReaderService> logger, IHttpClientFactory httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+
+        public List<T> ReadCSVFromFilePath<T, TMap>(string filePath) where TMap : ClassMap<T>
+        {
+            _logger.LogInformation("Searching for CSV file for processing");
+
+            var records = new List<T>();
+            if (File.Exists(filePath))
+            {
+                using var reader = new StreamReader(filePath);
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                csv.Context.RegisterClassMap<TMap>();
+                records = csv.GetRecords<T>().ToList();
+                Console.WriteLine($"Total Records Read: {records.Count}");
+            }
+            else
+            {
+                _logger.LogError("File not found: {FilePath}", filePath);
+            }
+            return records;
+        }
+
+        public async Task<List<T>> ReadCsvFileFromUrlAsync<T, TMap>(string urlFilePath) where TMap : ClassMap<T>
+        {
+            _logger.LogInformation("Downloading CSV file from url: {UrlFilePath}", urlFilePath);
+
+            var records = new List<T>();
+
+            try
+            {
+                var client = _httpClient.CreateClient();
+
+                var response = await client.GetAsync(urlFilePath);
+                response.EnsureSuccessStatusCode();
+
+                using var stream = await response.Content.ReadAsStreamAsync();
+                using var reader = new StreamReader(stream);
+                using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                csv.Context.RegisterClassMap<TMap>();
+                records = csv.GetRecords<T>().ToList();
+                _logger.LogInformation("Total Records Read: {Records}", records.Count);
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "HTTP request error downloading CSV file from url: {UrlFilePath}", urlFilePath);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error downloading CSV file from url: {UrlFilePath}", urlFilePath);
+            }
+
+            return records;
+        }
+    }
+}
+
