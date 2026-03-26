c74532ad0eba96c395ff8dc00d6bc8eb3994b386
hamzahshakeel-DfE <hamzah.shakeel@education.gov.uk>
2026-03-26 14:32:34 +0000

    [AWARD-1107] - Upgrade to NET10 (#63)
    * Initial start on net10 upgrade
    
    * upgraded pacakges
    
    * updated packages
    
    * Removed automapper

--- src/SFA.DAS.AODP.Jobs.Test/Application/Services/FundedQualificationWriterTests.cs
+++ src/SFA.DAS.AODP.Jobs.Test/Application/Services/FundedQualificationWriterTests.cs
@@ -1,5 +1,4 @@
 ﻿using AutoFixture;
-using AutoMapper;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
@@ -19,7 +18,6 @@ namespace SFA.DAS.AODP.Jobs.Test.Application.Services
         private readonly FunctionContext _functionContext;
         private ApplicationDbContext _dbContext;
         private Fixture _fixture;
-        private readonly IMapper _mapper;
         private Guid LifeCycleStageNew = new Guid("00000000-0000-0000-0000-000000000001");
         private Guid LifeCycleStageChanged = new Guid("00000000-0000-0000-0000-000000000002");
         private Guid ProcessStageNoAction = new Guid("00000000-0000-0000-0000-000000000001");
@@ -53,9 +51,6 @@ namespace SFA.DAS.AODP.Jobs.Test.Application.Services
                     new FundedQualificationOfferDTO() { Id = Guid.NewGuid(), Name = FundingOffer2, FundingAvailable = "True", FundingApprovalStartDate = DateTime.Now.AddYears(-1), FundingApprovalEndDate = DateTime.Now.AddYears(1) },
                     new FundedQualificationOfferDTO() { Id = Guid.NewGuid(), Name = FundingOffer3, FundingAvailable = "True", FundingApprovalStartDate = DateTime.Now.AddYears(-1), FundingApprovalEndDate = DateTime.Now.AddYears(1) },
                     new FundedQualificationOfferDTO() { Id = Guid.NewGuid(), Name = FundingOffer1, FundingAvailable = "True", FundingApprovalStartDate = DateTime.Now.AddYears(-1), FundingApprovalEndDate = DateTime.Now.AddYears(1) }));
-
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
-            _mapper = new Mapper(configuration);
         }
 
         [Fact]
@@ -352,8 +347,7 @@ namespace SFA.DAS.AODP.Jobs.Test.Application.Services
         {
             return new FundedQualificationWriter(
                 _loggerMock.Object,
-                _dbContext,
-                _mapper
+                _dbContext
             );
         }
 
