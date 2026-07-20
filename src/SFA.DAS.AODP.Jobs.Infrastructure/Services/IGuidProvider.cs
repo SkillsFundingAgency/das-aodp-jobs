aee57655a055dd5a7d26d96675c64e3619443be6
Hamzah Shakeel <hamzah.shakeel@education.gov.uk>
2026-07-20 11:56:00 +0100

    slight change to comment

--- src/SFA.DAS.AODP.Jobs.Infrastructure/Services/IGuidProvider.cs
+++ src/SFA.DAS.AODP.Jobs.Infrastructure/Services/IGuidProvider.cs
@@ -8,6 +8,7 @@ public interface IGuidProvider
     /// <summary>
     /// Generates a new guid.
     /// </summary>
+    /// <remarks>The name parameter is a simple way to identify the purpose of the generated guid, useful when mocking in unit tests to have more precise control over guid generation.</remarks>
     /// <returns>Returns a new guid.</returns>
     Guid NewGuidFor(string name);
 }
