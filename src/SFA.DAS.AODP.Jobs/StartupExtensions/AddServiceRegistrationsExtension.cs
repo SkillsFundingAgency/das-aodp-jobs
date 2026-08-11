f41d1062e71b22e20f81ad23ac2c65a81a59658a
khanna <karen@laveryhanna.com>
2026-08-11 16:23:29 +0100

    updated application context registration

--- src/SFA.DAS.AODP.Jobs/StartupExtensions/AddServiceRegistrationsExtension.cs
+++ src/SFA.DAS.AODP.Jobs/StartupExtensions/AddServiceRegistrationsExtension.cs
@@ -29,7 +29,8 @@ public static class AddServiceRegistrationsExtension
         services.Configure<StorageConfiguration>(configuration.GetSection(StorageConfiguration.SectionName));
 
         services.AddHttpClient("importPldns", clinet => clinet.Timeout = TimeSpan.FromMinutes(5));
-        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        services.AddScoped<IApplicationDbContext>(sp =>
+            sp.GetRequiredService<IDbContextFactory<ApplicationDbContext>>().CreateDbContext());
         services.AddScoped<IJobsRepository, JobsRepository>();
         services.AddScoped<IQualificationsService, QualificationsService>();
         services.AddTransient<IOfqualRegisterService, OfqualRegisterService>();
